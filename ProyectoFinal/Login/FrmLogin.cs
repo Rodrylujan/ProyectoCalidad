4dfc56ff9911940963b748baec8bc0ffa6850b30
Ingenieria <Ingenieria@UTSILAB412>
2023-10-11 15:54:57 -0500

    Login

---
+++ ProyectoFinal/Login/FrmLogin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal.Login
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+            lbIntentos.Visible = false;
+            lbIntentos.Visible = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txbPassword_MouseEnter(object sender, EventArgs e)
+        {
+            string condiciones = "";
+            condiciones = "- Minimo 8 caracteres\n" +
+                            "- Debe incluir un numero, una mayuscula y \n" +
+                            "    una minuscula\n" +
+                            "- Solo incluir los siguientes caracteres \n    especales: # @ %\n" +
+                            "- Minimo 8 caracteres\n";
+            lbContraseñas.Text = condiciones;
+        }
+
+        private void txbPassword_MouseLeave(object sender, EventArgs e)
+        {
+            lbContraseñas.Text = "";
+        }
+
+        private void lbCambiarPasss_Click(object sender, EventArgs e)
+        {
+            FrmCambiarPass frmCambiarPass = new FrmCambiarPass();
+            frmCambiarPass.Show();
+        }
+
+        private void lbOlvidePasss_Click(object sender, EventArgs e)
+        {
+            FrmOlvidePass frmOlvidePass = new FrmOlvidePass();
+            frmOlvidePass.Show();
+        }
+    }
+}
---
+++ ProyectoFinal/Login/FrmOlvidePass.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace ProyectoFinal.Login
+{
+    partial class FrmOlvidePass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(619, 12);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(120, 40);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Atras";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Se enio un codio de cuatro dicgitos a tu nuero de celular:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(250, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "XXX XXX X";
+            // 
+            // txbUser
+            // 
+            this.txbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUser.Location = new System.Drawing.Point(254, 146);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(99, 26);
+            this.txbUser.TabIndex = 11;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.FlatAppearance.BorderSize = 0;
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(243, 204);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(120, 40);
+            this.btnActualizar.TabIndex = 15;
+            this.btnActualizar.Text = "Siguiente";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // FrmOlvidePass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 345);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.txbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmOlvidePass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmOlvidePass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
